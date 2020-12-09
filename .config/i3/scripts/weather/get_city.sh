76126e485205f8ec1c290f643652868c5647a46b
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2020-12-09 19:25:20 +0100

    cleanup and add exit menu

---
+++ .config/i3/scripts/weather/get_city.sh
@@ -0,0 +1,9 @@
+PUBLIC_IP=$(wget -qO- "https://ipinfo.io/ip")  # public ip
+
+LOCATION=$(curl -s "https://tools.keycdn.com/geo.json?host=$PUBLIC_IP" | jq .data.geo.city | sed 's/"//g')  # lookup location based on ip
+
+LOCATION=${LOCATION,,} # to lower case
+
+echo $LOCATION
+#export MY_LOCATION="aachen"
+#export MY_LOCATION=$LOCATION
