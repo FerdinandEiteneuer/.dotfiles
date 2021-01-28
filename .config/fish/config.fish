03598d92317019ac0da4d50c9b9b1a2e0b327ed3
Ferdinand Eiteneuer <ferdinand.eiteneuer@rwth-aachen.de>
2021-01-28 11:38:24 +0100

    when creating new shell, automatically cd into the pwd of the last cd command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,14 @@ alias st "git status"
 alias cda "conda deactivate"
 alias ca "conda activate"
 
+
+# anytime the cd command is called, this handler gets executed
+# need to source this function for it to work, otherwise it wont work
+source ~/.config/fish/functions/_cd_handler.fish
+
+# use the env variable set by _cd_handler to auto cd into the last directory
+# that we changed into inside a different shell.
+# This way, we start in the same dir as the last shell.
+if set -q _cd_handler_last_pwd
+    cd $_cd_handler_last_pwd
+end
